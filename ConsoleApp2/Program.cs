a11ea1f99b80c7411c708e41ed1b7a6a411a7d8f
sabaripriy <85024788+sabaripriy@users.noreply.github.com>
2021-05-29 19:51:32 +0530

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Logical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a String : ");
+            string inputString = Console.ReadLine();
+            string resultString = string.Empty;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (!resultString.Contains(inputString[i]))
+                {
+                    resultString += inputString[i];
+                }
+            }
+            Console.WriteLine(resultString);
+
+            Console.ReadKey();
+        }
+    }
+}
